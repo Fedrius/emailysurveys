c0571d2ab59ce2b3475c49857597a08eee847682
Paul <paulmlee7@gmail.com>
2018-04-14 00:15:01 -0700

    Completed Google oauth setup

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/user');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30 days, 24 hours, ,60 min 60 sec 1000 ms
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
