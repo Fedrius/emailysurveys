4186b1878e4e6e066be97622e14ee5d7a90bb0b5
Paul <paulmlee7@gmail.com>
2018-04-15 01:29:48 -0700

    Finished auth flow with prod and dev keys.

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '709197406499-lhceakd4ddu2o8vd84poijnq93fatqoe.apps.googleusercontent.com',
+    googleClientSecret: '5ewhdZR31yMd3mBGkjFxyuYL',
+    mongoURI: 'mongodb://fedrius:Sunbro18@ds139448.mlab.com:39448/emaily-devtest',
+    cookieKey: 'iweuncvdsijfhnusmtrlkweojfdsnfsihgbfciushnkn'
+};
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//figuring out what set of cred to return
+if(process.env.NODE_ENV === 'production'){
+    //in production. return prod keys.
+    module.exports = require('./prod')
+} else {
+    //in dev. return dev keys
+    module.exports = require('./dev');
+}
